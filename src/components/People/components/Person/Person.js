0b161398017f41cc521be1ce2a82533b336e6691
rinkalsavaliya <rinkal@scaletech.xyz>
2019-04-02 10:29:15 +0530

    feature: radium issue

--- src/components/People/components/Person/Person.js
+++ src/components/People/components/Person/Person.js
@@ -1,32 +1,39 @@
 import React from 'react';
 import personClasses from './Person.module.css';
 import Radium from 'radium';
-
 /*
 * JSX to render person block
 */
-const person = (props) => {
-  // use of radium for sudo selectors
-  const btnStyle = {
-    ':hover': { color: 'grey' }
-  };
-  const personStyle = {
-    ':hover': { color: 'grey' },
-    '@media (max-width: 512px)': {
-      width: '450px'
-    }
-  };
-  return (
-    <div className={personClasses.person} key={props.id} style={personStyle}>
-      <h1>{props.name}</h1>
-      <p>Your Age : {props.age}</p>
-      <p className={(props.children) ? '' : personClasses.red}>{props.children || 'Hobby getting loaded..... '}</p>
-      <div className={personClasses['display-inline-flex']}>
-        <button className={personClasses.btn} onClick={props.shuffle}>Shuffle Others</button>
-        <button className={personClasses.btn} style={btnStyle} onClick={props.delete}>Delete</button><br/>
+class Person extends React.Component {
+  static getDerivedStateFromProps = (props, state) => {
+    console.log('getDerivedStateFromProps :', props, state);
+    return props;
+  }
+  // shouldComponentUpdate = (props, state) => {
+  //   console.log('shouldComponentUpdate', this.props.id === props.id, this.props.id, props.id);
+  //   if (this.props.id === props.id) {
+  //     return false;
+  //   }
+  //   return true;
+  // }
+  render() {
+    // this use of radium styled hover makes the life cycle run again
+    // if you comment the hover part (line), you won't get **getDerivedStateFromProps** called
+    const personStyle = {
+      ':hover': { color: 'red' }
+    };
+    return (
+      <div key={this.props.id} className={personClasses.person} style={personStyle}>
+        <h1>{this.props.name}</h1>
+        <p>Your Age : {this.props.age}</p>
+        <p className={(this.props.children) ? '' : personClasses.red}>{this.props.children || 'Hobby getting loaded..... '}</p>
+        <div className={personClasses['display-inline-flex']}>
+          <button className={personClasses.btn} onClick={this.props.shuffle}>Shuffle Others</button>
+          <button className={personClasses.btn + ' ' + personClasses['dlt-btn']} onClick={this.props.delete}>Delete</button><br/>
+        </div>
       </div>
-    </div>
-  )
+    )
+  }
 };
 
-export default Radium(person);
+export default Radium(Person);
